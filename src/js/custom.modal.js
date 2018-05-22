956b4fc50f46ed760d02de4cb09d196b9678b409
Tatiana Fokina <fokinatatian@gmail.com>
2018-05-23 01:21:40 +0300

    Переключение работ в модальном окне (не полностью)

--- src/js/custom.modal.js
+++ src/js/custom.modal.js
@@ -1,15 +1,25 @@
+// Открытие и закрытие модального окна
 $(function() {
-	//var $scrolled_element = '.panel-inr'; // Селектор прокручивающегося блока
+	
+	var $modal = ".modal-container"; // Селектор модального окна. Элемент, который показыаем при нажатии на ссылку.
+	var $link = '.work-card'; // Селектор ссылки на модальное окно
+	var $link_opened = 'js-opened'; // Класс, который добавляем ссылки, соответствующей открытому модальному окну
 
-	// После клика на пункт окно разворачивается
-	$('.work-card').click(function () {
-		$('html').css('overflow', 'hidden');
-		$('.modal-container').css('display', 'flex');
+	// Убирает все классы --opened у всех ссылок
+	function removeAllOpenedFlags(){
+		$($link).removeClass($link_opened);
+	}
 
-		var $modal_content_src = $(this).data("work"); // Считывает название файла с работой (атрибут data-work)
+	// Добавляет класс для этой ссылки
+	function setAsOpened($taget){
+		$($taget).addClass($link_opened); // Добавляет класс для этой ссылки
+	}
 
+
+	// Функция загрузки и вставки контента из внешнего файла
+	function loadToModal($fileName){
 		$.ajax({
-			url: 'ajax-portfolio/' + $modal_content_src + '.html',
+			url: 'ajax-portfolio/' + $fileName + '.html',
 			success: function(data) {
 				var $title = $(data).filter('#title').html();
 				$('.modal__title').html($title);
@@ -27,58 +37,90 @@ $(function() {
 				$('.modal__content').html($content);
 			}
 		});
+	}
 
 
 
-		//$('.panel .scrollbar-inner').scrollTop(0); // Пролистывает в начало
+
+	/*===============
+	Открытие окна после клика на ссылку
+	===============*/
+	$($link).click(function () {
+		$('html').css('overflow', 'hidden');
+		$($modal).css('display', 'flex')
+			.scrollTop(0); // Пролистывает в начало
+
+		var $modal_content_src = $(this).data("work"); // Считывает название файла с работой (атрибут data-work)
+
+		loadToModal($modal_content_src);
+		removeAllOpenedFlags();
+		setAsOpened(this);
 	});
 
-	// Закрытие окна
-	$('.modal__close-btn, .modal-container').click(function () {
-		$('.modal-container').css('display', 'none');
-		$('html').css('overflow', 'auto');
+
+
+
+	/*===============
+	Закрытие окна
+	===============*/
+
+	// Функция закрытия окна
+	function modal_closure(){
+		$($modal).css('display', 'none'); // Скрывает окно
+		$('html').css('overflow', 'auto'); // Возвращает скроллбары
+		removeAllOpenedFlags(); // Убирает все классы js--opened у всех ссылок
+	}
+
+	// Нажатие на кнопку закрытия
+	$('.modal__close-btn').click(function () {
+		modal_closure();
 	});
 
-
-	//Закрыть после клика на .modal-container (включая дочерние элементы)
-	$('.modal-container').click(function() {
-		//$(this).removeClass('d-inline-block');
-		$('html').css('overflow', 'auto');
+	// Закрыть после клика на .modal-container (включая дочерние элементы)
+	$($modal).click(function() {
+		modal_closure();
 	});
 	//НЕ закрывать при клике на .modal
 	$('.modal').click(function(event){
 		event.stopPropagation();
 	});
+
+
+
+
+
+	/*===============
+	Переключатель работ
+	===============*/
+
+	$('.modal__pager__next').click(function () {
+		var $modal_contentNext = $('.' + $link_opened).closest('div').next().children($link); // Следующая работа
+		var $modal_contentNext_src = $modal_contentNext.data("work"); // Считывает атрибут data-work у следующей работы
+
+		var $modal_contentPrev_src = $('.' + $link_opened).closest('div').prev().children($link).data("work"); // Считывает атрибут data-work у предыдущей работы
+
+		//$($link_opened);
+		var $links_amount = $($link).length; // Кол-во ссылок
+		//console.log($modal_contentNext_src);
+
+
+		removeAllOpenedFlags(); // Убирает все классы js--opened у всех ссылок
+		loadToModal($modal_contentNext_src);
+		$modal_contentNext.addClass($link_opened); // + класс для ссылки
+
+
+
+	});
+
+
+
 });
 
 
 
-/*
-* Modal
-* */
-/*jQuery(document).ready(function ($) {
-	$('.link-to-modal').click(function(){
-		//$(this).next('.modal-container').addClass('d-inline-block');
-		var popup_id=$(this).attr('aria-labelledby');
-		$('.' + popup_id).addClass('d-inline-block');
-		$('html').css('overflow', 'hidden');
-
-	});
 
 
-	// Закрыть
-	$('.modal__close-btn').click(function(){
-		$('html').css('overflow', 'auto');
-		$(this).closest('.modal-container').removeClass('d-inline-block');
-	});
 
-	//Закрыть после клика на .modal-container (включая дочерние элементы)
-	$('.modal-container').click(function() {
-		$(this).removeClass('d-inline-block');
-		$('html').css('overflow', 'auto');
-	});
-	//НЕ закрывать при клике на .modal
-	$('.modal').click(function(event){
-		event.stopPropagation();
-	});
-});*/
+
+
+
