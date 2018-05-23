9d0e781164c2cfeb72ccc2bcb3573f6873b1f9e6
Tatiana Fokina <fokinatatian@gmail.com>
2018-05-23 14:51:44 +0300

    Листатель модальных окон, доработка окон, правки в сетке stylus, правки переменных для цветов

--- src/js/custom.modal.js
+++ src/js/custom.modal.js
@@ -9,13 +9,10 @@ $(function() {
 	function removeAllOpenedFlags(){
 		$($link).removeClass($link_opened);
 	}
-
 	// Добавляет класс для этой ссылки
 	function setAsOpened($taget){
 		$($taget).addClass($link_opened); // Добавляет класс для этой ссылки
 	}
-
-
 	// Функция загрузки и вставки контента из внешнего файла
 	function loadToModal($fileName){
 		$.ajax({
@@ -31,7 +28,12 @@ $(function() {
 				$('.modal__desc').html($desc);
 
 				var $link = $(data).filter('#link').html();
-				$('.modal__link').html($link);
+				if ($link){
+					$('.modal__link').html($link);
+				}
+				else {
+					$('.modal__link').empty();
+				}
 
 				var $content = $(data).filter('#content').html();
 				$('.modal__content').html($content);
@@ -93,34 +95,62 @@ $(function() {
 	Переключатель работ
 	===============*/
 
+
+
+	var $modal_now = 1,
+		$links_amount = $($link).length,
+		$modal_contentFirst = $($link).first(),
+		$modal_contentFirst_src = $modal_contentFirst.data("work"),
+		$modal_contentLast = $($link).last(),
+		$modal_contentLast_src = $modal_contentLast.data("work");
+
 	$('.modal__pager__next').click(function () {
-		var $modal_contentNext = $('.' + $link_opened).closest('div').next().children($link); // Следующая работа
-		var $modal_contentNext_src = $modal_contentNext.data("work"); // Считывает атрибут data-work у следующей работы
+		var $modal_contentNext = $('.' + $link_opened).closest('div').next().children($link),
+			$modal_contentNext_src = $modal_contentNext.data("work");
 
-		var $modal_contentPrev_src = $('.' + $link_opened).closest('div').prev().children($link).data("work"); // Считывает атрибут data-work у предыдущей работы
+		// Для перехода к первому элементу
+		if ($modal_now === $links_amount || $modal_now <= 0 || $modal_now > $links_amount) {
+			loadToModal($modal_contentFirst_src); // Вставляем содержимое из первой ссылки
+			$modal_now = 1; // Сбрасываем счётчик
+			removeAllOpenedFlags(); // - классы для всех ссылок
+			$modal_contentFirst.addClass($link_opened); // + класс для ссылки
+		}
+		// Для перехода к следующему элементу
+		else {
+			loadToModal($modal_contentNext_src);
+			$modal_now++;
+			removeAllOpenedFlags(); // - классы для всех ссылок
+			$modal_contentNext.addClass($link_opened); // + класс для ссылки
+		}
+		$($modal).scrollTop(0); // Пролистывает в начало
 
-		//$($link_opened);
-		var $links_amount = $($link).length; // Кол-во ссылок
-		//console.log($modal_contentNext_src);
-
-
-		removeAllOpenedFlags(); // Убирает все классы js--opened у всех ссылок
-		loadToModal($modal_contentNext_src);
-		$modal_contentNext.addClass($link_opened); // + класс для ссылки
-
-
-
+		console.log($modal_now);
 	});
 
 
+	$('.modal__pager__prev').click(function () {
+		var $modal_contentPrev = $('.' + $link_opened).closest('div').prev().children($link),
+			$modal_contentPrev_src = $modal_contentPrev.data("work");
+
+		// Для перехода к последнему элементу
+		if ($modal_now === 1 || $modal_now <= 0 || $modal_now > $links_amount) {
+			loadToModal($modal_contentLast_src);
+			$modal_now = $links_amount; // Устанавливаем максимальное значение
+			removeAllOpenedFlags(); // - классы для всех ссылок
+			$modal_contentLast.addClass($link_opened); // + класс для ссылки
+		}
+		// Для перехода к предыдущему элементу
+		else {
+			loadToModal($modal_contentPrev_src);
+			$modal_now--;
+			removeAllOpenedFlags(); // - классы для всех ссылок
+			$modal_contentPrev.addClass($link_opened); // + класс для ссылки
+
+		}
+		$($modal).scrollTop(0); // Пролистывает в начало
+
+		console.log($modal_now);
+	});
+
 
 });
-
-
-
-
-
-
-
-
-
