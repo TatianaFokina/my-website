1461dfb620d459dd68700fc59e09367c0ea12680
Tatiana Fokina <fokinatatian@gmail.com>
2018-05-23 23:46:15 +0300

    Плавный скролл, адаптивность .hero-photo

---
+++ src/js/custom.anchor-link-scroll.js
@@ -0,0 +1,13 @@
+$(function($) {
+
+	$(function () {
+
+		$('.nav-item[href^="#"]').click(function () {
+			$('html, body').animate({
+				scrollTop: $( $(this).attr('href') ).offset().top
+			}, 800);
+			return false;
+		});
+	});
+
+});
