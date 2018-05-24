c83a90ac5569ec5ec144ba3a10adea71c4c5c03f
Tatiana Fokina <fokinatatian@gmail.com>
2018-05-25 01:54:45 +0300

    Фикс для плавного скролла к якорю

--- src/js/custom.anchor-link-scroll.js
+++ src/js/custom.anchor-link-scroll.js
@@ -1,13 +1,7 @@
-$(function($) {
-
-	$(function () {
-
-		$('.nav-item__lnk[href^="#"]').click(function () {
-			$('html, body').animate({
-				scrollTop: $( $(this).attr('href') ).offset().top
-			}, 800);
-			return false;
-		});
-	});
-
+$('.nav-item__lnk[href*="#"]')
+	.not('[href="#"]').not('[href="#0"]').click(function () {
+	$('html, body').animate({
+		scrollTop: $( $(this).attr('href') ).offset().top
+	}, 800);
+	return false;
 });
